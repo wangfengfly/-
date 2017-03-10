a2b5bfd52e19b119fa819149ab95e77f067389f5
wangfeng211731 <wangfeng211731@sogou-inc.com>
2017-03-10 14:45:44 +0800

    归并排序，从底向上解法

--- merge_sort.php
+++ merge_sort.php
@@ -4,6 +4,8 @@
  * User: wangfeng
  * Date: 2017/3/10
  * Time: 10:40
+ * 空间复杂度O(n)
+ * 时间复杂度 O(nlogn)
  */
 
 /*
@@ -50,6 +52,26 @@ function merge($arr, $begin, $mid, $end){
     return $res;
 }
 
+/*
+ * bottom-up 解法
+ */
+function bottomup(&$arr){
+    $n = count($arr);
+    for($width=1; $width<$n; $width=2*$width){
+        for($i=0;$i<$n;$i+=2*$width){
+            $begin = $i;
+            $right = min($i+$width, $n-1);
+            $end = min($i+2*$width, $n-1);
+            $res = merge($arr, $begin, $right, $end);
+            array_splice($arr, $begin, $end-$begin+1, $res);
+        }
+    }
+
+}
+
 $arr = array(1,6,5,2,9,3,11);
 $arr = split_sort($arr, 0, count($arr)-1);
+//var_dump($arr);
+$arr = array(1,6,5,2,9,3,11);
+bottomup($arr);
 var_dump($arr);
