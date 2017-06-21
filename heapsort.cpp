245ac8460ea49ecefff74536cf68c323557ca1cc
wangfeng211731 <wangfeng211731@sogou-inc.com>
2017-06-21 10:56:15 +0800

    heapsort

---
+++ heapsort.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+void swap(int *a, int *b){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void heapify(int arr[], int n, int i){
+    int l = 2*i+1, r=2*i+2, largest = i;
+    if(l<n && arr[l]>arr[largest]){
+        largest = l;
+    }
+    if(r<n && arr[r]>arr[largest]){
+        largest = r;
+    }
+    if(largest!=i){
+        swap(arr+i, arr+largest);
+        heapify(arr, n, largest);
+    }
+}
+
+void heapSort(int arr[], int n){
+    //堆是完全二叉树，完全二叉树的特性最左边子树不为空，如果有右子树
+    for(int i=n/2-1; i>=0; i--){
+        heapify(arr, n, i);
+    }
+    for(int i=n-1; i>=0; i--){
+        swap(&arr[0], &arr[i]);
+        heapify(arr, i, 0);
+    }
+}
+
+int main(){
+    int arr[] = {12, 11, 13, 5, 6, 7};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    heapSort(arr, n);
+    
+    for(int i=0; i<n; i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
