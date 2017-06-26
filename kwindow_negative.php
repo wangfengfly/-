bfe6b5bd4dd0339e1f33d8c7f66817f1ad723380
wangfeng211731 <wangfeng211731@sogou-inc.com>
2017-06-26 11:45:09 +0800

    k window中第一个负数

--- kwindow_negative.php
+++ kwindow_negative.php
@@ -6,7 +6,27 @@
  * Time: 11:02
  * 给定一个数组和k， 输出窗口大小为k的序列中第一个负数
  */
-
+/*
+ * O（(n-k+1)k)
+ */
+function kwindow1($arr, $k){
+    $n = count($arr);
+    for($i=0; $i<$n-$k+1; $i++){
+        for($j=0; $j<$k; $j++){
+            if($arr[$i+$j]<0){
+                echo $arr[$i+$j].' ';
+                break;
+            }
+        }
+        if($j>=$k) {
+            echo '0 ';
+        }
+    }
+    echo "\n";
+}
+/*
+ * O(n)
+ */
 function kwindow($arr, $k){
     $n = count($arr);
     $queue = new SplQueue();
@@ -38,3 +58,4 @@ function kwindow($arr, $k){
 
 $arr = array(12, -1, -7, 8, -15, 30, 16, 28);
 kwindow($arr, 3);
+kwindow1($arr, 3);
