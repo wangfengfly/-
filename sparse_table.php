58510a70dfc753c96de03458d81cb5a0822703c8
wangfeng <wangfeng211731@sogou-inc.com>
2017-12-26 12:08:10 +0800

    sparse table

---
+++ sparse_table.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Author: wangfeng
+ * Date: 2017/12/26
+ * Time: 10:36
+ * 求数组任意[l,r]范围内的最小值
+ * O(nlgn)
+ * 空间O(nlgn)
+ */
+
+function build(array $arr){
+    if(!$arr){
+        return false;
+    }
+    $n = count($arr);
+    $lookup = array();
+    for($i=0; $i<$n; $i++){
+        $lookup[$i][0] = $arr[$i];
+    }
+
+    for($j=1; (1<<$j)<=$n; $j++){
+        for($i=0; $i+(1<<($j-1))<$n; $i++){
+            if($lookup[$i][$j-1] <= $lookup[$i+(1<<($j-1))][$j-1]){
+                $lookup[$i][$j] = $lookup[$i][$j-1];
+            }else{
+                $lookup[$i][$j] = $lookup[$i+(1<<($j-1))][$j-1];
+            }
+        }
+    }
+
+    return $lookup;
+}
+
+function query($l, $r, $lookup){
+    $n = floor(log($r-$l+1, 2));
+    if($lookup[$l][$n] <= $lookup[$r-(1<<$n)+1][$n]){
+        return $lookup[$l][$n];
+    }else{
+        return $lookup[$r-(1<<$n)+1][$n];
+    }
+}
+
+$arr = array( 7, 2, 3, 0, 5, 10, 3, 12, 18);
+$lookup = build($arr);
+var_dump($lookup);
+var_dump(query(0,4,$lookup));
+var_dump(query(4,7,$lookup));
+var_dump(query(7,8,$lookup));
