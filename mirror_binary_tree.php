ab5a879006b1eccd91f7a28f9fdefe6c09a7d44b
wangfeng <wangfeng211731@sogou-inc.com>
2019-02-28 17:30:58 +0800

    二叉树镜像操作，自定义实现循环队列

---
+++ mirror_binary_tree.php
@@ -0,0 +1,121 @@
+<?php
+/**
+ * Author: wangfeng
+ * Date: 2019/2/28
+ * Time: 16:23
+ * 二叉树镜像操作，自定义实现循环队列
+ */
+
+class Node{
+    public $left;
+    public $right;
+    public $data;
+
+    public function __construct($left, $right, $data){
+        $this->left = $left;
+        $this->right = $right;
+        $this->data = $data;
+    }
+}
+
+class Queue{
+    public $data;
+    public $maxsize;
+    public $head;
+    public $tail;
+
+    public function __construct($maxsize){
+        $this->maxsize = $maxsize;
+        $this->head = $this->tail = 0;
+        $this->data = [];
+    }
+
+    public function isEmpty(){
+        return $this->head == $this->tail;
+    }
+
+    public function isFull(){
+        return (($this->tail+1)%$this->maxsize) == $this->head;
+    }
+
+    public function enQueue($item){
+        if($this->isFull()){
+            echo "Queue is Full\n";
+            return false;
+        }
+
+        $this->data[$this->tail] = $item;
+        $this->tail = ($this->tail+1)%$this->maxsize;
+        return true;
+    }
+
+    public function deQueue(){
+        if($this->isEmpty()){
+            echo "Queue is Empty\n";
+            return false;
+        }
+
+        $item = $this->data[$this->head];
+        $this->head = ($this->head+1)%$this->maxsize;
+        return $item;
+    }
+
+}
+
+class Tree{
+
+    public $root;
+    public $size;
+
+    public function __construct(array $data){
+        if(!$data){
+            return;
+        }
+
+        $n = count($data);
+        $this->size = $n;
+        $this->root = new Node(null, null, $data[0]);
+        $temp = $this->root;
+        $q = new Queue($n);
+        $q->enQueue($temp);
+        $i = 0;
+        while(!$q->isEmpty()){
+            $temp = $q->deQueue();
+            if(($i+1) < $n){
+                $left = new Node(null, null, $data[$i+1]);
+                $temp->left = $left;
+                $q->enQueue($left);
+                $i++;
+            }
+            if(($i+1) < $n){
+                $right = new Node(null, null, $data[$i+1]);
+                $temp->right = $right;
+                $q->enQueue($right);
+                $i++;
+            }
+        }
+    }
+
+    public function mirror(){
+        $q = new Queue($this->size);
+        $q->enQueue($this->root);
+        while(!$q->isEmpty()){
+            $item = $q->deQueue();
+            $temp = $item->left;
+            $item->left = $item->right;
+            $item->right = $temp;
+            if($item->left) {
+                $q->enQueue($item->left);
+            }
+            if($item->right) {
+                $q->enQueue($item->right);
+            }
+        }
+    }
+
+}
+
+$tree = new Tree([1,2,3,4,5,6]);
+var_dump($tree);
+$tree->mirror();
+var_dump($tree);
